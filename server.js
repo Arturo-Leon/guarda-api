53f32b415ce48205e9d2b438d2d43bc39ab62f26
Arturo-Leon <guszetinev@hotmail.com>
2026-07-26 21:55:12 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -434,3 +434,8 @@ openDb().then(() => {
         console.log('🚀 API SQLite corriendo en puerto ' + PORT);
     });
 });
+
+app.use(express.static(path.join(__dirname, 'frontend')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
